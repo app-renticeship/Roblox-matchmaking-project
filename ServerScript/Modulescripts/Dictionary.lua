6b19876ee4d3ec34f43c96b3700314433e17cbb3
Dante <43237761+app-renticeship@users.noreply.github.com>
2019-06-30 11:54:54 +0700

    Import

---
+++ ServerScript/Modulescripts/Dictionary.lua
@@ -0,0 +1,28 @@
+local Dictionary = 
+{
+	[0] = "==================\n",
+	[1] = " SetAsync Success",
+	[2] = " SetAsync Failed on join",
+	[3] = " GetAsync Failed on join",
+	[4] = " Data Exists and fetched",
+	[5] = " DataStore Failed to Load, Please Rejoin",
+	
+	
+	[6] = " Added To Queue",
+	[7] = " Removed From Queue",
+	[8] = "[S] Trying to Matchmake",
+	[9] = "[S] Matchmaking: ",
+	[10] = "[S] Searching Opponent",
+	[11] = "[S] Not Enough Queued",
+	
+	
+	[12] = "ILLEGAL REQUEST!\n(stop your exploit)" ,
+	[13] = " Might be using exploit\n[S] Illegal Request",
+	[14] = "Roblox DataStore is OFFLINE\nMatch will not be saved",
+		
+	
+}
+
+
+
+return Dictionary
